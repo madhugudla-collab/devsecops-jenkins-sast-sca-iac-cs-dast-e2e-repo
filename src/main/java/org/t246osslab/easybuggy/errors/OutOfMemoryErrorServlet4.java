9b47246558f732cff32fda13248ec5a6391d87b1
madhugudla-collab <madhu.gudla@gmail.com>
2026-03-18 00:04:58 -0400

    [Security Bot] Save and re-use this "Random".: fix OutOfMemoryErrorServlet4.java

--- src/main/java/org/t246osslab/easybuggy/errors/OutOfMemoryErrorServlet4.java
+++ src/main/java/org/t246osslab/easybuggy/errors/OutOfMemoryErrorServlet4.java
@@ -2,7 +2,7 @@ package org.t246osslab.easybuggy.errors;
 
 import java.io.IOException;
 import java.util.Properties;
-import java.util.Random;
+import java.security.SecureRandom; // SECURITY FIX: Use SecureRandom for better randomness
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -15,11 +15,12 @@ import org.t246osslab.easybuggy.core.servlets.AbstractServlet;
 @WebServlet(urlPatterns = { "/oome4" })
 public class OutOfMemoryErrorServlet4 extends AbstractServlet {
 
+    private final SecureRandom r = new SecureRandom(); // SECURITY FIX: Save and re-use this SecureRandom
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
 
         Properties properties = System.getProperties();
-        Random r = new Random();
         while (true) {
             properties.put(r.nextInt(), "value");
         }
